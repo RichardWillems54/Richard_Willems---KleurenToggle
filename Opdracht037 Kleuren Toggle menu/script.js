687d82ef7958fce3b695e208e0ca85ac3c3c3bef
RichardWillems54 <76516026+RichardWillems54@users.noreply.github.com>
2021-06-10 13:54:11 +0200

    Add files via upload

--- Opdracht037 Kleuren Toggle menu/script.js
+++ Opdracht037 Kleuren Toggle menu/script.js
@@ -1,38 +1,39 @@
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 const home = document.getElementById("home");
-const rood = document.getElementById("rood");
-const oranje = document.getElementById("oranje");
-const paars = document.getElementById("paars");
-const groen = document.getElementById("groen");
+const rood = document.getElementById("red");
+const oranje = document.getElementById("orange");
+const paars = document.getElementById("purple");
+const groen = document.getElementById("green");
 
 menu.style.visibility = "hidden";
 
 
 hamburger.addEventListener("mouseover", function () {
     menu.style.visibility = "visible";
-    home.addEventListener("click", function () {
-        document.body.style.backgroundColor = "gray";
-        menu.style.visibility = "hidden";
-    });
+});
 
-    rood.addEventListener("click", function () {
-        document.body.style.backgroundColor = "red";
-        menu.style.visibility = "hidden";
-    });
+home.addEventListener("click", function () {
+    document.body.style.backgroundColor = "gray";
+    menu.style.visibility = "hidden";
+});
 
-    oranje.addEventListener("click", function () {
-        document.body.style.backgroundColor = "orange";
-        menu.style.visibility = "hidden";
-    });
+rood.addEventListener("click", function () {
+    document.body.style.backgroundColor = "red";
+    menu.style.visibility = "hidden";
+});
 
-    paars.addEventListener("click", function () {
-        document.body.style.backgroundColor = "purple";
-        menu.style.visibility = "hidden";
-    });
+oranje.addEventListener("click", function () {
+    document.body.style.backgroundColor = "orange";
+    menu.style.visibility = "hidden";
+});
 
-    groen.addEventListener("click", function () {
-        document.body.style.backgroundColor = "green";
-        menu.style.visibility = "hidden";
-    });
+paars.addEventListener("click", function () {
+    document.body.style.backgroundColor = "purple";
+    menu.style.visibility = "hidden";
+});
+
+groen.addEventListener("click", function () {
+    document.body.style.backgroundColor = "green";
+    menu.style.visibility = "hidden";
 });
